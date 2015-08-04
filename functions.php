dfac63877e55c26b3ff87bbf1a6786850208378b
tdoria <doria.tiago@gmail.com>
2015-08-03 21:07:45 -0400

    SASS

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function theme_styles(){
 
-	wp_enqueue_style( 'main', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'main', get_template_directory_uri() . '/css/application.css' );
 }
 add_action( 'wp_enqueue_scripts', 'theme_styles');
 
